535d9d1ab0ca4e44ee51e58308969ad7020fcaf7
Ercument Ekentok <ercument.ekentok@ozu.edu.tr>
2020-05-27 01:08:45 +0300

    adds game and player main attributes

--- src/Game.py
+++ src/Game.py
@@ -1,1 +1,35 @@
 # This class holds the functions and attributes of game rules, time control and scoring. 
+import Player as p
+
+player_list = []
+
+class Game:
+
+    def __init__(self):
+        self.number_of_players = int(input("Enter the number of players: "))
+        self.number_of_rounds = int(input("Enter the number of rounds: "))
+        self.game_type = input("Enter the type of game: ")
+        self.time_limit = 30
+    
+    def addPlayers(self):
+        for i in range(self.number_of_players):
+            name = input("Enter your username: ")
+            player_list.append(p.Player(name, i,0))
+
+Game().addPlayers()
+
+'''
+    def __init__(self, number_of_players, number_of_rounds, game_type, time_limit):
+        self.number_of_players = number_of_players
+        self.number_of_rounds = number_of_rounds
+        self.game_type = game_type
+        self.time_limit = time_limit
+
+        
+    def createGame(self):
+        size = input("Enter the number of players: ")
+        length =  input("Enter the number of rounds: ")
+        mode = input("Enter the type of game: ")
+        limit = 30
+        return Game(size, length, mode, limit)
+'''
--- src/Player.py
+++ src/Player.py
@@ -1,1 +1,15 @@
 # This class holds the functions and attributes of players.
+
+class Player:
+
+    def __init__(self, username, player_id, score):
+        self.username = username
+        self.player_id = player_id
+        self.score = score
+
+# Player samples
+p1 = Player("ehemingway", 1, 0)
+p2 = Player("szweig", 2, 0)
+
+print(p1.username + " id = " + str(p1.player_id) + ". His score is " + str(p1.score))
+print(p2.username + " id = " + str(p2.player_id) + ". His score is " + str(p2.score))
